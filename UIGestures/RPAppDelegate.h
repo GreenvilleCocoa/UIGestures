c7d149350059073e6c8ca7b0baab6b021fd3a58c
Ryan Poolos <ryanpoolos@Gmail.com>
2012-06-13 18:58:40 -0400

    Initial Commit

---
+++ UIGestures/RPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RPAppDelegate.h
+//  UIGestures
+//
+//  Created by Ryan Poolos on 6/13/12.
+//  Copyright (c) 2012 Frozen Fire Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIGestures/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIGestures
+//
+//  Created by Ryan Poolos on 6/13/12.
+//  Copyright (c) 2012 Frozen Fire Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RPAppDelegate class]));
+    }
+}
